85e83b165817fbe32d9f00ee9e0b090914975425
Arber Totraku <arbertotraku@gmail.com>
2024-01-24 20:56:03 +0100

    SCSS installiert und structure gebaut

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter(
         routes: [
             {
                 path: '/',
-                component: () => Home
+                component: Home
             },
             {
                 path: '/about',
