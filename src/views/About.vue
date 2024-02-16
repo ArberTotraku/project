b3fccfa8ae8005b0a6296f382920b095595ab687
Arber Totraku <arbertotraku@gmail.com>
2024-02-16 16:27:07 +0100

    small change

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,11 +2,7 @@
 
     <main id="about-page">
         <h1>About</h1>
-        <p>Dies ist ein Projekt für das Space Squad, an dem ich viel Spaß hatte.
-
-Normalerweise ist es nicht das beste Dashboard, das man sehen kann, aber ich habe versucht, in diesen Tagen so viel wie möglich umzusetzen. Bitte beachten Sie, dass ich nicht allzu viel mit Vue.js gearbeitet habe (ich habe mehr mit React gearbeitet) und in nur diesen Tagen habe ich versucht herauszufinden, wie die Syntax ist und wie ich alles zusammen verbinden kann. Ich habe viel Zeit damit verbracht, Bugs zu beheben, die unterwegs waren, und dies ist das Ergebnis, zu dem ich gekommen bin. Normalerweise weiß ich, dass es viele Verbesserungen zu machen gibt, aber ich werde versuchen, sie zu beheben und sie in naher Zukunft zu lernen, was ich hoffe, mit Ihnen zu sein!
-
-Vielen Dank für die Aufmerksamkeit, fröhliches Programmieren und weniger Bugs! :)</p>
+        <p>About page</p>
     </main>
     
     </template>
